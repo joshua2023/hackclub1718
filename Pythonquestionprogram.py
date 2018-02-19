ae1ef8b9d32328f0108199b25229f25d9384923a
Joshua Snitgen <jsnitgen@icloud.com>
2018-02-19 15:35:37 +0000

    adding python question program

---
+++ Pythonquestionprogram.py
@@ -0,0 +1,13 @@
+from tkinter import *
+import random
+import time
+tk = Tk()
+tk.title("Bounce")
+tk.resizable(0, 0)
+tk.wm_attributes("Joshua Snitgen", 1)
+canvas = Canvas(tk, width=500, heigt=400, bd=0, highlightthickness=0)
+canvas.pack()
+tk.update()
+
+                 
+
