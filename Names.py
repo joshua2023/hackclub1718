710ad346bd2ea51a485d95ea90f50a81afb363dc
Joshua Snitgen <jsnitgen@icloud.com>
2018-02-19 15:56:55 +0000

    Python names file

---
+++ Names.py
@@ -0,0 +1,166 @@
+#Program is 160 lines
+
+print("Hello. I am so excited to see you!! Today I will be testing your knowledge on trivia. Type start() to start the program")
+
+def start():#This program askes each of my family members a trivia question based on what they say their name is
+
+        myName = input("What is your name?")
+
+        if(myName == "Joshua"):#Joshua Question
+            print("Hi, Joshua! Hope you are doing well today!")
+            question=input("Would you like to hear your question(yes or no)? Type in lowercase")
+            if(question == "yes"):
+                yes=input("How much-in US dollars-did it cost to build the International Space Station? Type in Standard Form")
+                if(yes == "$150,000,000,000"):
+                      print("Yay! You got it!! See you later!")
+                else:
+                    print("Sorry, the correct answer is $150,000,000,000. Hope to see you again soon!")
+        
+            else:
+                (question == "no")
+                print("Ok. Bye then!")
+                exit()
+
+        elif(myName == "joshua"):#joshua Question
+            print("Hi, Joshua! Hope you are doing well today!")
+            question=input("Would you like to hear your question(yes or no)? Type in lowercase.")#Not sure why when I type "Yes" while running the program, it says "Ok, Bye then"
+            if(question == "yes"):
+                yes=input("How much-in US dollars-did it cost to build the International Space Station? Type in Standard Form")
+                if(yes == "$150,000,000,000"):
+                    print("Yay! You got it!! See you later!")
+                else:
+                    print("Sorry, the correct answer is $150,000,000,000. Hope to see you again soon!")
+            else:
+                (question == "no")
+                print("Ok. Bye then!")
+                exit()
+            
+
+        elif(myName == "Doug"):#Doug Question
+           print("Hi, Doug! Hope you are doing well today!")
+           question=input("Would you like to hear your question(yes or no)? Type in lowercase")
+           if(question == "yes"):
+               yes=input("What is the swirling disk of light around a black hole called? A black hole ____________ disk. Type in lowercase")
+               if(yes == "accretion"):
+                   print("Yay! You got it!! See you later!")
+               else:
+                    print("Sorry, the correct answer is accretion. Hope to see you again soon!")
+                
+           else:
+                (question == "no")
+                print("Ok, Bye then!")
+                exit()
+
+        elif(myName == "doug"):#doug Question
+            print("Hi, Doug! Hope you are doing well today!")
+            question=input("Would you like to hear your question(yes or no)? Type in lowercase")
+            if(question == "yes"):
+                yes=input("What is the swirling disk of light around a black hole called? A black hole ____________ disk. Type in lowercase.")
+                if(yes=="accretion"):
+                    print("Yay! You got it!! See you later!")
+                else:
+                    print("Sorry, the correct answer is is accretion> HOpe to see you again soon!")
+            else:
+                (question == "no")
+                print("Ok, Bye then!")
+                exit()
+            
+
+
+        elif(myName == "Emily"):#Emily Question
+           print("Hi, Emily! Hope you are doing well today!")
+           question=input("Would you like to hear your question(yes or no)? Type in lowercase")
+           if(question == "yes"):
+               yes=input("What is it called during a solar eclipse where the moon's shadow blocks the sun compleately? Type in lowercase")
+               if(yes == "umbra"):
+                    print("Yay! You got it!! See you later!")
+               else:
+                   print("Sorry, the correct answer is umbra. Hope to see you again soon!")
+           else:
+               (question == "no")
+               print("Ok, Bye then!")
+               exit()
+
+        elif(myName == "emily"):#emily Question
+            print("Hi, Emily! Hope you are doing well today!")
+            question=input("WOukd you like to hear your question(yes or no)? Type in lowercase")
+            if(question == "yes"):
+                yes=input("What is it called duringf a solar eclipse where the moon's shadow blocks the sunb compleatlt? Type in lowecase")
+                if(yes == "umbra"):
+                    print("Yay! You got it!! See you later!")
+                else:
+                    print("Sorry, the correct answer in umbra. Hope to see you again soon!")
+            else:
+                (question == "no")
+                print("Ok, Bye then!")
+                exit()
+
+        elif(myName == "Grace"):#Grace Question
+           print("Hi, Grace! Hope you are doing well today!")
+           question=input("Would you like to hear your question(yes or no)? Type in lowercase")
+           if(question == "yes"):
+               yes=input("What does Schrodinger's Cat Parodox involve? A cat and a ___________. Type in lowercase")
+               if(yes == "box"):
+                   print("Yay! You got it!! See you later!")
+               else:
+                   print("Sorry, the correct answer is box. Hope to see you again soon!")
+           else:
+               (question == "no")
+               print("Ok, Bye then!")
+               exit()
+
+        elif(myName == "grace"):#grace Question
+            print("Hi, Grace! Hope you are doing well today!")
+            question=input("Would you like to hear your question(yes or no)? Type in lowercase")
+            if(question == "yes"):
+                yes=input("What does Schrodinger's Cat Parodox involve? A cat and a ___________. Type in lowercase")
+                if(yes == "box"):
+                    print("Yay! You got it!! See you later!")
+                else:
+                    print("Sorry, the correct answer is box. Hope to see you again soon!")
+            else:
+                (question == "no")
+                print("Ok, Bye then!")
+                exit()
+
+        else:#Else Question
+            print("Hi, someone whose name I dont recognise! Hope you are doing well today!")
+            question=input("Would you like to hear your question(yes or no)? Type in lowercase")
+            if(question == "yes"):
+                yes=input("What are the four neucleotides in RNA? Adenine, Cytosine, Guanine, and ________. Type in lowercase")
+                if(yes == "uracil"):
+                    print("Yay! You got it!! See you later!")
+                else:
+                    print("Sorry, the correct answer is uracil. Hope to see you again soon!")
+            else:
+                (question == "no")
+                print("Ok, Bye then!")
+                exit()
+
+        
+                
+        restart=input("Do you want to start again? Type in lowercase").lower()
+        if restart == "yes":
+            start()
+
+        else:
+              sure=input("Are you sure???")
+              if(sure == "yes"):
+                  print("Nope, not letting you leave!!! HAHAHAHAHA")
+                  fine=input("Do you really, REALLY want to leave(yes or no) Type in lowercase")
+                  if(fine == "yes"):
+                      print("FINE THEN!!!!!")
+                      exit()
+                  else:
+                     (fine == "no")
+                  print("Ok, you can start again:)")
+                  start()
+
+              elif(sure == "no"):
+                  print("Ill let you start again:)")
+                  print("Enjoy!")
+                  start()
+
+
+
+
