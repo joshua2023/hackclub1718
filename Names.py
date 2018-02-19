b9bb6050fec301af45ea4b610c5974e0be178a93
Joshua Snitgen <jsnitgen@icloud.com>
2018-02-19 21:29:05 +0000

    Trivia Questions Program

--- Names.py
+++ Names.py
@@ -1,13 +1,19 @@
-#Program is 160 lines
+#This is my very first program. Program is 217 lines
 
 print("Hello. I am so excited to see you!! Today I will be testing your knowledge on trivia. Type start() to start the program")
 
 def start():#This program askes each of my family members a trivia question based on what they say their name is
 
         myName = input("What is your name?")
+        
 
         if(myName == "Joshua"):#Joshua Question
-            print("Hi, Joshua! Hope you are doing well today!")
+            print("Hi, Joshua! Hope you are doing well today! If you are wondering, the current time is-")
+
+            import time
+            print(time.asctime())
+            g=input("Press g to continue")
+            
             question=input("Would you like to hear your question(yes or no)? Type in lowercase")
             if(question == "yes"):
                 yes=input("How much-in US dollars-did it cost to build the International Space Station? Type in Standard Form")
@@ -20,9 +26,15 @@ def start():#This program askes each of my family members a trivia question base
                 (question == "no")
                 print("Ok. Bye then!")
                 exit()
+                
 
         elif(myName == "joshua"):#joshua Question
-            print("Hi, Joshua! Hope you are doing well today!")
+            print("Hi, Joshua! Hope you are doing well today! If you are wondering, the current time is-")
+
+            import time
+            print(time.asctime())
+            g=input("Press g to continue")
+            
             question=input("Would you like to hear your question(yes or no)? Type in lowercase.")#Not sure why when I type "Yes" while running the program, it says "Ok, Bye then"
             if(question == "yes"):
                 yes=input("How much-in US dollars-did it cost to build the International Space Station? Type in Standard Form")
@@ -34,10 +46,17 @@ def start():#This program askes each of my family members a trivia question base
                 (question == "no")
                 print("Ok. Bye then!")
                 exit()
+                
             
 
         elif(myName == "Doug"):#Doug Question
-           print("Hi, Doug! Hope you are doing well today!")
+           print("Hi, Doug! Hope you are doing well today! If you are wondering, the current time is-")
+
+           import time
+           print(time.asctime())
+           g=input("Press g to continue")
+
+           
            question=input("Would you like to hear your question(yes or no)? Type in lowercase")
            if(question == "yes"):
                yes=input("What is the swirling disk of light around a black hole called? A black hole ____________ disk. Type in lowercase")
@@ -50,9 +69,15 @@ def start():#This program askes each of my family members a trivia question base
                 (question == "no")
                 print("Ok, Bye then!")
                 exit()
+                
 
         elif(myName == "doug"):#doug Question
-            print("Hi, Doug! Hope you are doing well today!")
+            print("Hi, Doug! Hope you are doing well today! If you are wondering, the current time is-")
+
+            import time
+            print(time.asctime())
+            g=input("Press g to continue")
+            
             question=input("Would you like to hear your question(yes or no)? Type in lowercase")
             if(question == "yes"):
                 yes=input("What is the swirling disk of light around a black hole called? A black hole ____________ disk. Type in lowercase.")
@@ -68,7 +93,12 @@ def start():#This program askes each of my family members a trivia question base
 
 
         elif(myName == "Emily"):#Emily Question
-           print("Hi, Emily! Hope you are doing well today!")
+           print("Hi, Emily! Hope you are doing well today! If you are wondering, the current time is-")
+
+           import time
+           print(time.asctime())
+           g=input("Press g to continue")
+           
            question=input("Would you like to hear your question(yes or no)? Type in lowercase")
            if(question == "yes"):
                yes=input("What is it called during a solar eclipse where the moon's shadow blocks the sun compleately? Type in lowercase")
@@ -80,9 +110,15 @@ def start():#This program askes each of my family members a trivia question base
                (question == "no")
                print("Ok, Bye then!")
                exit()
+               
 
         elif(myName == "emily"):#emily Question
-            print("Hi, Emily! Hope you are doing well today!")
+            print("Hi, Emily! Hope you are doing well today! If you are wondering, the current time is-")
+
+            import time
+            print(time.asctime())
+            g=input("Press g to continue")
+            
             question=input("WOukd you like to hear your question(yes or no)? Type in lowercase")
             if(question == "yes"):
                 yes=input("What is it called duringf a solar eclipse where the moon's shadow blocks the sunb compleatlt? Type in lowecase")
@@ -94,9 +130,15 @@ def start():#This program askes each of my family members a trivia question base
                 (question == "no")
                 print("Ok, Bye then!")
                 exit()
+                
 
         elif(myName == "Grace"):#Grace Question
-           print("Hi, Grace! Hope you are doing well today!")
+           print("Hi, Grace! Hope you are doing well today! If you are wondering, the current time is-")
+
+           import time
+           print(time.asctime())
+           g=input("Press g to continue")
+           
            question=input("Would you like to hear your question(yes or no)? Type in lowercase")
            if(question == "yes"):
                yes=input("What does Schrodinger's Cat Parodox involve? A cat and a ___________. Type in lowercase")
@@ -108,9 +150,15 @@ def start():#This program askes each of my family members a trivia question base
                (question == "no")
                print("Ok, Bye then!")
                exit()
+               
 
         elif(myName == "grace"):#grace Question
-            print("Hi, Grace! Hope you are doing well today!")
+            print("Hi, Grace! Hope you are doing well today! If youy are wondering, the current time is-")
+
+            import time
+            print(time.asctime())
+            g=input("Press g to continue")
+            
             question=input("Would you like to hear your question(yes or no)? Type in lowercase")
             if(question == "yes"):
                 yes=input("What does Schrodinger's Cat Parodox involve? A cat and a ___________. Type in lowercase")
@@ -122,9 +170,15 @@ def start():#This program askes each of my family members a trivia question base
                 (question == "no")
                 print("Ok, Bye then!")
                 exit()
+                
 
         else:#Else Question
-            print("Hi, someone whose name I dont recognise! Hope you are doing well today!")
+            print("Hi, someone whose name I dont recognise! Hope you are doing well today! If you are wondering, the current time is-")
+
+            import time
+            print(time.asctime())
+            g=input("Press g to continue")
+            
             question=input("Would you like to hear your question(yes or no)? Type in lowercase")
             if(question == "yes"):
                 yes=input("What are the four neucleotides in RNA? Adenine, Cytosine, Guanine, and ________. Type in lowercase")
@@ -138,7 +192,7 @@ def start():#This program askes each of my family members a trivia question base
                 exit()
 
         
-                
+        #RESTART PROGRAM PROMPT     
         restart=input("Do you want to start again? Type in lowercase").lower()
         if restart == "yes":
             start()
@@ -160,6 +214,7 @@ def start():#This program askes each of my family members a trivia question base
                   print("Ill let you start again:)")
                   print("Enjoy!")
                   start()
+        #RESTART PROGRAM PROMPT
 
 
 
